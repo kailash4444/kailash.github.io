b5a99ff628b72df77d212e7e4471fea0de08d827
kailash narreddy <narreddykailash@outlook.com>
2025-04-29 23:40:28 +0530

    updated github url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'kailash.github.io', // Replace <repository-name> with your GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
